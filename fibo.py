9dfbe96701f48e228657ec998ed7ee1d7bf6d98e
achew8 <70230739+achew8@users.noreply.github.com>
2020-09-02 19:43:31 -0700

    Add files via upload

---
+++ fibo.py
@@ -0,0 +1,23 @@
+Prev2=0
+Prev1=1
+for i in range(20):
+    Current=Prev2+Prev1
+    print(Current)
+    Prev2, Prev1=Prev1, Current
+
+x=12
+if x > 10:
+    print("x is greater than 10")
+
+x=9
+if x > 9:
+    print("x is greater than 9")
+
+x=4
+x==5
+x==4
+x!=4
+
+
+
+
